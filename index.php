1060ca385096f3ce0b7771cb1f301cd5c04d50b9
genesisd1005 <sanchezgenesisd@gmail.com>
2022-06-29 17:02:11 -0400

    completado boostrap 1

--- index.php
+++ index.php
@@ -111,109 +111,6 @@
     ?>
 
 
-
-  
-
-    <div class="form_continer" >
-        <h2 class="text-center">Suscripcion</h2>
-        <form  method="GET" action="?" >
-            <table class="form_table" >
-                <tr>
-                    <td><span>Email o numero de telefono:</span></td>
-                    <td><input type="text" name="suscripcion" /></td>
-                </tr>
-                <tr>
-                    <td><span>Contraseña:</span></td>
-                    <td><input type="password" name="contraseña"/></td>
-                </tr>
-                <tr>
-                    <td><span>Planes:</span></td>
-                    <td>
-                        <select type="text" name="planes" id="">
-                            <option value="">-</option>
-                            <option value="p1">Plan basico 5$</option>
-                            <option value="p2">Plan estandar 10$</option>
-                            <option value="p3">Plan premium 13$</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td><br><input type="submit" name="enviar" value="Aceptar"/></td>
-                </tr>
-            </table>
-        </form>
-    </div>
-
-
-
-    
-
-  <div class= "container">
-
-  <div class="form_continer">
-        <h2 class="text-center">Nuevo Registro</h2>
-        <form  method="GET" action="?" >
-            <table class="form_table">
-                <tr>
-                    <td><span>Nombre: </span></td>
-                    <td colspan="5"><input class="imput_total" type="text" name="nombre" maxlength="20" required/><br></td>
-                </tr>
-                <tr>
-                    <td><span>Apellido: </span></td>
-                    <td colspan="5"> <input class="imput_total" type="text" name="apellido" maxlength="20" required/></td>
-                </tr>
-                <tr>
-                    <td><span>Cedula: </span></td>
-                    <td>
-                        <select name="nacionalidad" required>
-                            <option value="">-</option>
-                            <option value="E">E-</option>
-                            <option value="V">V-</option>
-                        </select>
-                    </td>
-                    <td colspan="3">
-                        <input class="imput_total" type="text" name="cedula" maxlength="11" required/>
-                    </td>
-                <tr>
-                    <td><span>Pais: </span></td>
-                    <td colspan="4">
-                        <select class="imput_total" type="text" name="pais" required>
-                            <option value="">-</option>
-                            <option value="ARG">Argentina</option>
-                            <option value="CH">Chile</option>
-                            <option value="CO">Colombia</option>
-                            <option value="VE">Venezuela</option>
-
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td><span>Genero: </span></td>
-                    <td><input type="radio" name="genero" value="f" required /></td>
-                    <td><span>Femenino: </span></td>
-                    <td><input type="radio" name="genero" value="m" required/></td>
-                    <td><span>Masculino: </span></td>
-                </tr>
-                <tr>
-                    <td><span>Acepto: </span></td>
-                    <td><input type="checkbox" name="condiciones" id="condiciones" onClick="isActive()" required/></td>
-                    <td colspan="4"><input type="button" name="leer" value="Leer terminos" /></td>
-                </tr>
-                
-                    <input type="hidden" name="id_user" value="1" /></td>
-                
-                <tr >
-                    
-                    <td colspan="6" class="text-center" style="padding-top:10px"><input id="enviar" type="submit" name="enviar" value="Registrar" disabled />
-                    <input type="reset" name="reset" value="Limpiar" /></td>
-                </tr>
-            </table>
-        </form>
-    </div>
-        <br>
-
-
-
 <div class="container">
 
 
@@ -222,31 +119,64 @@
         <div class="col col-lg-6">
             <form>
                 <div class="mb-3" >
-                    <h2 class="text-center">Nuevo Plan</h2>
-                    <label for="exampleInputEmail1" class="form-label">Plan</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <h2 class="text-center">Nuevo Registro</h2>
+                    <label for="exampleInputEmail1" class="form-label">Nombre:</label>
+                    <input type="email" class="form-control" id="nombre" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Precio</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <label for="exampleInputPassword1" class="form-label">Apellido:</label>
+                    <input type="text" class="form-control" id="apellido">
                     </div>
-                    <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Aceptar terminos</label>
+                    <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Cedula:</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                        <option value="">-</option>
+                        <option value="v">V</option>
+                        <option value="e">E</option>
+                    </select>
+                    <input type="text" class="form-control" id="cedula">
+                    </div>
+                    <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Pais:</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                        <option value="">-</option>
+                        <option value="ARG">Argentina</option>
+                        <option value="CH">Chile</option>
+                        <option value="CO">Colombia</option>
+                        <option value="VE">Venezuela</option>
+                    </select>
+                    </div>
+                    <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Genero:</label>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                    <label class="form-check-label" for="inlineRadio1">Femenino.</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                    <label class="form-check-label" for="inlineRadio2">Masculino.</label>
+                    </div>
+                    <br>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        Aceptar terminos.
+                    </label>
+                    </div>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right">
+                    Leer terminos.
+                    </button>
+                    <div>
+                    <button class="btn btn-primary" type="submit" value="Submit">Enviar</button>
+                    <button class="btn btn-primary" type="reset" value="Reset">Limpiar</button>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Enviar
-
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="red" d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>
-
-
-                </button>
             </form>
         </div>
         <div class="col"></div>
     </div>
 
-
-
+    <br>
 
 
   <div class="row">
@@ -260,7 +190,7 @@
                 </div>
                 <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Precio</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <input type="text" class="form-control" id="exampleInputPassword1">
                 </div>
                 <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -277,26 +207,27 @@
     <div class="col col-lg-6">
         <form>
             <div class="mb-3" >
-                <h2 class="text-center">Nuevo Plan</h2>
-                <label for="exampleInputEmail1" class="form-label">Plan</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <h2 class="text-center">Suscripcion</h2>
+                <label for="exampleInputEmail1" class="form-label">Cedula:</label>
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                        <option value="">-</option>
+                        <option value="v">V</option>
+                        <option value="e">E</option>
+                </select>
+                <input type="text" class="form-control" id="cedula">
                 </div>
                 <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Precio</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="exampleInputPassword1" class="form-label">Plan:</label>
+                <input type="text" class="form-control" id="exampleInputPassword1">
                 </div>
-                <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Aceptar terminos</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+            <button type="submit" class="btn btn-primary">Guardar</button>
          </form>
     </div>
     <div class="col"></div>
   </div>
 
 
-
+<br>
 
 
 
@@ -313,7 +244,7 @@
 
                 <td>PLAN</td>
                 <td>PRECIO</td>
-                <td>Acciomes</td>
+                <td>Acciones</td>
             </tr>
             <?php 
 
@@ -325,7 +256,10 @@
                 <td><? echo $planes["nombre_plan"]?></td>
                 <td><? echo $planes["precio"]?></td>
                 <td>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM31.1 128H416V448C416 483.3 387.3 512 352 512H95.1C60.65 512 31.1 483.3 31.1 448V128zM111.1 208V432C111.1 440.8 119.2 448 127.1 448C136.8 448 143.1 440.8 143.1 432V208C143.1 199.2 136.8 192 127.1 192C119.2 192 111.1 199.2 111.1 208zM207.1 208V432C207.1 440.8 215.2 448 223.1 448C232.8 448 240 440.8 240 432V208C240 199.2 232.8 192 223.1 192C215.2 192 207.1 199.2 207.1 208zM304 208V432C304 440.8 311.2 448 320 448C328.8 448 336 440.8 336 432V208C336 199.2 328.8 192 320 192C311.2 192 304 199.2 304 208z"/></svg>
+                <i class="fa-solid fa-trash-can"></i>
+                </td>
+                <td>
+                <i class="fa-solid fa-pen-to-square"></i>
                 </td>
             </tr>
             <?php 
@@ -336,42 +270,15 @@
     </div>
   </div>
 
-</div>
-
-
-
-
-
-    
-        <!--<div class="form_continer" >
-        <h2 class="text-center">Nuevo Plan</h2>
-        <form  method="GET" action="?" >
-            <table class="form_table" >
-                <tr>
-                    <td><span>Plan:</span></td>
-                    <td><input type="text" name="plan" maxlength="20" required/></td>
-                </tr>
-                <tr>
-                    <td><span>Precio:</span></td>
-                    <td><input type="text" name="precio" maxlength="5" placeholder="00.00" required/></td>
-                </tr>
-                <tr>
-                    <td class="text-center"><br><input type="submit" name="enviar_plan" value="Registrar"/>
-                    <input type="reset" name="reset" value="Limpiar"/><td>
-                </tr>
-            </table>
-        </form>
-    </div>-->
-
-    <div class="columna">
-
-        <table class= "table">
-            <thead>
+  <div class="row">
+    <div class="col col-lg-10 offset-lg-1">
+        <table class= "table table-striped table-hover">
+        <thead>
                 <tr>
                     <td colspan="6">Listado de clientes</td>
                 </tr>
             </thead>
-            <tr>
+             <tr>
                 <td colspan="3">Clientes</td>
                 <td rowspan="2">Suscripcion</td>
                 <td colspan="2">Planes</td>
@@ -384,8 +291,6 @@
                 <td>PRECIO</td>
             </tr>
             <?php 
-
-            
             while($clientes=mysql_fetch_array($query)){
             ?>
             <tr>
@@ -402,6 +307,14 @@
             
         </table>
     </div>
+
+</div>
+
+           
+           
+
+            
+            
 
 </div>
 
